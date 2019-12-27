5f5759c29e5cf97c0200f0b0e33ce7a213ddde7f
Vishwas <gopinav5@gmail.com>
2019-12-27 20:27:32 +0530

    Icon context

--- react-icons-demo/src/App.js
+++ react-icons-demo/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './App.css'
+import { IconContext } from 'react-icons'
 import { FaReact } from 'react-icons/fa'
+import { MdAlarm } from 'react-icons/md'
 
 function App () {
   return (
-    <div className='App'>
-      <FaReact color='purple' size='10rem' />
-    </div>
+    <IconContext.Provider value={{ color: 'blue', size: '5rem' }}>
+      <div className='App'>
+        <FaReact />
+        <MdAlarm color='purple' size='10rem' />
+      </div>
+    </IconContext.Provider>
   )
 }
 
