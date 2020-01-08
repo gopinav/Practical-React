e4263e252c73b86f78a0047298758eb511c3c108
Vishwas <gopinav5@gmail.com>
2020-01-08 19:12:24 +0530

    Add react-toastify

--- react-notifications-demo/src/App.js
+++ react-notifications-demo/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-function App() {
+function App () {
+  const notify = () => toast('Wow so easy !')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={notify}>Notify !</button>
+      <ToastContainer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
