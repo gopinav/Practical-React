1f9afa4e8ac8ce3b311cdf2def7db5533f427a32
Vishwas <gopinav5@gmail.com>
2020-01-11 13:03:17 +0530

    Custom component

--- react-notifications-demo/src/App.js
+++ react-notifications-demo/src/App.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import './App.css'
-import { ToastContainer, toast } from 'react-toastify'
+import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+
+toast.configure()
+
+const CustomToast = ({ closeToast }) => {
+  return (
+    <div>
+      Something went wrong! <button onClick={closeToast}>Close</button>
+    </div>
+  )
+}
 
 function App () {
   const notify = () => {
-    toast('Wow so easy !', { position: toast.POSITION.TOP_LEFT })
-    toast('Wow so easy !', { position: toast.POSITION.TOP_CENTER })
-    toast('Wow so easy !', { position: toast.POSITION.TOP_RIGHT })
-    toast('Wow so easy !', { position: toast.POSITION.BOTTOM_LEFT })
-    toast('Wow so easy !', { position: toast.POSITION.BOTTOM_CENTER })
+    toast('Default!', { position: toast.POSITION.TOP_LEFT })
+    toast.success('Success!', {
+      position: toast.POSITION.TOP_CENTER,
+      autoClose: 8000
+    })
+    toast.info('Info!', {
+      position: toast.POSITION.TOP_RIGHT,
+      autoClose: false
+    })
+    toast.warn(<CustomToast />, {
+      position: toast.POSITION.BOTTOM_LEFT
+    })
+    toast.error('Error!', { position: toast.POSITION.BOTTOM_CENTER })
     toast('Wow so easy !', { position: toast.POSITION.BOTTOM_RIGHT })
   }
   return (
     <div className='App'>
       <button onClick={notify}>Notify !</button>
-      <ToastContainer />
     </div>
   )
 }
