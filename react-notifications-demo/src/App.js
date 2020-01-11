297f46f90bf83a946b5889bea72e540aea3496d8
Vishwas <gopinav5@gmail.com>
2020-01-11 09:15:10 +0530

    Toast position

--- react-notifications-demo/src/App.js
+++ react-notifications-demo/src/App.js
@@ -4,7 +4,14 @@ import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 function App () {
-  const notify = () => toast('Wow so easy !')
+  const notify = () => {
+    toast('Wow so easy !', { position: toast.POSITION.TOP_LEFT })
+    toast('Wow so easy !', { position: toast.POSITION.TOP_CENTER })
+    toast('Wow so easy !', { position: toast.POSITION.TOP_RIGHT })
+    toast('Wow so easy !', { position: toast.POSITION.BOTTOM_LEFT })
+    toast('Wow so easy !', { position: toast.POSITION.BOTTOM_CENTER })
+    toast('Wow so easy !', { position: toast.POSITION.BOTTOM_RIGHT })
+  }
   return (
     <div className='App'>
       <button onClick={notify}>Notify !</button>
