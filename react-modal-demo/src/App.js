e09d149966427b91bcb33c00acec26db601930ca
Vishwas <gopinav5@gmail.com>
2020-01-14 20:58:29 +0530

    Styling

--- react-modal-demo/src/App.js
+++ react-modal-demo/src/App.js
@@ -8,7 +8,19 @@ function App () {
   return (
     <div className='App'>
       <button onClick={() => setModalIsOpen(true)}>Open modal</button>
-      <Modal isOpen={modalIsOpen}>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          overlay: {
+            backgroundColor: 'grey'
+          },
+          content: {
+            color: 'orange'
+          }
+        }}
+        // shouldCloseOnOverlayClick={false}
+      >
         <h2>Modal title</h2>
         <div>Modal Body</div>
         <div>
