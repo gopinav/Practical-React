bd6bca5df6e1001b86401274b81d6423d33524d7
Vishwas <gopinav5@gmail.com>
2020-02-09 07:12:11 +0530

    Add color picker component

--- react-color-demo/src/App.js
+++ react-color-demo/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { ChromePicker } from 'react-color'
+import './App.css'
 
-function App() {
+function App () {
+  const [color, setColor] = useState('#fff')
+  const [showColorPicker, setShowColorPicker] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => setShowColorPicker(showColorPicker => !showColorPicker)}
+      >
+        {showColorPicker ? 'Close color picker' : 'Pick a color'}
+      </button>
+      {showColorPicker && (
+        <ChromePicker
+          color={color}
+          onChange={updatedColor => setColor(updatedColor.hex)}
+        />
+      )}
+      <h2>You picked {color}</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
