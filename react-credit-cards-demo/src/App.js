48369124c4b6dda1f9c694e453c0e580296d6483
Vishwas <gopinav5@gmail.com>
2020-02-11 17:09:07 +0530

    Add Cards component

--- react-credit-cards-demo/src/App.js
+++ react-credit-cards-demo/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Cards from 'react-credit-cards'
+import './App.css'
+import 'react-credit-cards/es/styles-compiled.css'
 
-function App() {
+function App () {
+  const [number, setNumber] = useState('')
+  const [name, setName] = useState('')
+  const [expiry, setExpiry] = useState('')
+  const [cvc, setCvc] = useState('')
+  const [focus, setFocus] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Cards
+        number={number}
+        name={name}
+        expiry={expiry}
+        cvc={cvc}
+        focused={focus}
+      />
+      <form>
+        <input
+          type='tel'
+          name='number'
+          placeholder='Card Number'
+          value={number}
+          onChange={e => setNumber(e.target.value)}
+          onFocus={e => setFocus(e.target.name)}
+        />
+        <input
+          type='text'
+          name='name'
+          placeholder='Name'
+          value={name}
+          onChange={e => setName(e.target.value)}
+          onFocus={e => setFocus(e.target.name)}
+        />
+        <input
+          type='text'
+          name='expiry'
+          placeholder='MM/YY Expiry'
+          value={expiry}
+          onChange={e => setExpiry(e.target.value)}
+          onFocus={e => setFocus(e.target.name)}
+        />
+        <input
+          type='tel'
+          name='cvc'
+          placeholder='CVC'
+          value={cvc}
+          onChange={e => setCvc(e.target.value)}
+          onFocus={e => setFocus(e.target.name)}
+        />
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
