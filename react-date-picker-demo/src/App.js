0e1ca44c82282987dc5f6f3724258754e9cfb640
Vishwas <gopinav5@gmail.com>
2020-02-21 21:30:56 +0530

    Basic example

--- react-date-picker-demo/src/App.js
+++ react-date-picker-demo/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
-function App() {
+function App () {
+  const [selectedDate, setSelectedDate] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <DatePicker
+        selected={selectedDate}
+        onChange={date => setSelectedDate(date)}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
