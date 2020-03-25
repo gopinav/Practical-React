e831a72a290acf420522c8fd43670ba1370ac99f
Vishwas <gopinav5@gmail.com>
2020-03-25 12:59:56 +0530

    Add spinners

--- react-spinners-demo/src/App.js
+++ react-spinners-demo/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { css } from '@emotion/core'
+import { BounceLoader, BarLoader, BeatLoader } from 'react-spinners'
+import './App.css'
 
-function App() {
+const loaderCSS = css`
+  margin-top: 25px;
+  margin-bottom: 25px;
+`
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BounceLoader css={loaderCSS} size={48} color='orange' loading />
+      <BarLoader css={loaderCSS} size={48} color='orange' loading />
+      <BeatLoader css={loaderCSS} size={48} color='orange' loading />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
